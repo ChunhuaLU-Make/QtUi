ef63966f2ec0bb0967639682ec8f75385f233990
ChunhuaLU-Make <1692470640@qq.com>
2023-05-30 20:25:04 +0800

    界面制作还不行，有待提高

--- qt/UI_Obj/mainwindow.cpp
+++ qt/UI_Obj/mainwindow.cpp
@@ -2,13 +2,27 @@
 #include "ui_mainwindow.h"
 #include <QDebug>
 #include <QMessageBox>
+#include <QPalette>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    connect(ui->pushButton, &QPushButton::clicked,this, &MainWindow::MyPrintf);
+
+    /* Set background picture. */
+    this->setStyleSheet("border-image: url(:/ui_picture/Rabbit.jpg)");
+
+
+    QPalette palette = ui->lineEdit->palette();
+    //palette.setColor(QPalette::Base, QColor(255, 0, 0));
+    palette.setColor(QPalette::Base, QColor(255, 0, 0, 255));// # 设置背景颜色不透明
+    ui->lineEdit->setAutoFillBackground(true);
+    ui->lineEdit->setPalette(palette);
+
+
+
+    //connect(ui->pushButton, &QPushButton::clicked,this, &MainWindow::MyPrintf);
 
     this->readXml = NULL;
 }
