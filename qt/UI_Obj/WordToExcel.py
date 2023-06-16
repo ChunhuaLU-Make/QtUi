f16743612391bc08683f50dcb0280c2ff61ac365
ChunhuaLU-Make <1692470640@qq.com>
2023-06-16 11:41:07 +0800

    环境完成提交

--- qt/UI_Obj/WordToExcel.py
+++
@@ -1,9 +0,0 @@
-# This Python file uses the following encoding: utf-8
-
-# if __name__ == "__main__":
-#     pass
-
-def waht():
-    print("hell dog")
-    print("hell error");
-    print("hello good.");
